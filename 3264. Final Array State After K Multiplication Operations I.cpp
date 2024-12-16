733b7cfbc2ce1afef1d890f758208ac00a0ddf29
Khushnor Rahman Meem <khushnorrahmanmeem@gmail.com>
2024-12-16 07:55:05 +0600

    16-Dec-24

---
+++ 3264. Final Array State After K Multiplication Operations I.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int n=nums.size();
+       while(k--){
+        int id=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<nums[id]){
+                id=i;
+            }
+        }
+        nums[id] *= multiplier;
+       } 
+       return nums;
+    }
+};
